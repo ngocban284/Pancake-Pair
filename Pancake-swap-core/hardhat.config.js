c399b9aca300d85ad2fd28127bb8b8090b84c108
ngocban284 <daongocban1102@gmail.com>
2022-07-15 10:36:45 +0700

    update config

--- Pancake-swap-core/hardhat.config.js
+++ Pancake-swap-core/hardhat.config.js
@@ -7,9 +7,7 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: [
-        "0xa0c31ec3759513cbdcb60bd0d3f30d298bcede28c06c5dd3b77b2b8219158de6",
-      ],
+      accounts: [`${process.env.PRIVATE_KEY}`],
     },
   },
   solidity: "0.5.16",
--- testRouter/hardhat.config.js
+++ testRouter/hardhat.config.js
@@ -8,9 +8,7 @@ module.exports = {
       chainId: 97,
       gas: 6500000,
       gasPrice: 20000000000,
-      accounts: [
-        "0xa0c31ec3759513cbdcb60bd0d3f30d298bcede28c06c5dd3b77b2b8219158de6",
-      ],
+      accounts: [`${process.env.PRIVATE_KEY}`],
     },
   },
   solidity: {
